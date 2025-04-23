6e1c553bf4b448c2694fcfb1502fd07a34e13e0d
Avinash Jat <dev.avinashjat@gmail.com>
2025-04-23 08:00:20 +0530

    update frontend

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -23,8 +23,7 @@ function TopDoctors() {
             <div onClick={()=>{navigate(`/appointement/${item._id}`); scrollTo(0,0)}} key={index} className=" border border-blue-200 cursor-pointer rounded-xl overflow-hidden hover:translate-y-[-10px] transition-all duration-500">
               <img className="bg-blue-50 " src={item.image} alt="dcotor_img" />
               <div className="p-4">
-                <div className={`flex items-center text-center gap-2 text-sm ${item.available ? ' text-green-500' : 'text-gray-500' }`}>
-                  
+                <div className={`flex items-center text-center gap-2 text-sm ${item.available ? ' text-green-500' : 'text-gray-500' }`}> 
                    <p className={`w-2 h-2 ${item.available ? 'bg-green-500' : 'bg-gray-500' } rounded-full `}></p><p>{item.available ? 'Available': 'Not Available'}</p>
                 </div>
                 <p className="text-lg font-medium text-gray-900">{item.name}</p>
